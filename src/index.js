c4226db94bdb6f3353ed2b7c8bb7e6e9be2bc0a3
lsky <clin520@126.com>
2018-11-13 09:33:26 +0800

    add deepclone&isEmpty&toUnique

--- src/index.js
+++ src/index.js
@@ -4,8 +4,37 @@ let tools = {
         let _type = Object.prototype.toString.call(data),
             startIndex = _type.indexOf(' '),
             endIndex = _type.indexOf(']');
-        return _type.substring((startIndex + 1), endIndex);
+        return _type.substring((startIndex + 1), endIndex).toLowerCase();
+    },
+    deepClone (obj){
+    	if(tools.getType(obj) != 'object' || tools.getType(obj) != 'array'){
+    		return obj;
+    	}
+
+    	return JSON.parse(JSON.stringify(obj));
+    },
+    isEmpty (val) {
+    	switch(tools.getType(val)){
+    		case "object":
+    			if(Object.getOwnPropertyNames(val).length === 0){
+    				return true;
+    			}
+    			break;
+    		case 'array':
+    			if(val.length === 0 || (Object.getOwnPropertyNames(val).length === 1)){
+    				return true;
+    			}
+    			break;
+    		default:
+    			return val === '' || val === null || isNaN(val) || val === false;
+    	}
+    },
+    toUnique (data){
+    	let arr = [];
+    	if(tools.getType(data) === 'array'){
+    		arr = [ ...new Set(data) ];
+    	}
+
+    	return arr;
     }
 }
-
-console.log(tools.getType(new Date()));
