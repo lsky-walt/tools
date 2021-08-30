ab38aee89191798b523c2ff36873e200717f4b89
lsky-walt <lsky_walt@163.com>
2021-08-30 11:47:18 +0800

    update readme

--- src/browser.js
+++ src/browser.js
@@ -26,4 +26,37 @@ function generateIntent(config, options) {
   return `intent://${urlPath}/${intentTail}`
 }
 
-export { buildScheme, generateIntent }
+const getBrowser = () => {
+  const ua = window.navigator.userAgent || "",
+    isAndroid = /android/i.test(ua),
+    isIos = /iphone|ipad|ipod/i.test(ua),
+    isWechat = /MicroMessenger\/([\d.]+)/i.test(ua),
+    isWeibo = /(weibo).*weibo__([\d.]+)/i.test(ua),
+    isQQ = /qq\/([\d.]+)/i.test(ua),
+    isQzone = /qzone\/.*_qz_([\d.]+)/i.test(ua),
+    // 安卓 chrome 浏览器，很多 app 都是在 chrome 的 ua 上进行扩展的
+    isOriginalChrome =
+      /chrome\/[\d.]+ Mobile Safari\/[\d.]+/i.test(ua) &&
+      isAndroid &&
+      ua.indexOf("Version") > 0,
+    // chrome for ios 和 safari 的区别仅仅是将 Version/<VersionNum> 替换成了 CriOS/<ChromeRevision>
+    // ios 上很多 app 都包含 safari 标识，但它们都是以自己的 app 标识开头，而不是 Mozilla
+    isSafari =
+      /safari\/([\d.]+)$/i.test(ua) &&
+      isIos &&
+      ua.indexOf("Crios") < 0 &&
+      ua.indexOf("Mozilla") === 0
+
+  return {
+    isAndroid,
+    isIos,
+    isWechat,
+    isWeibo,
+    isQQ,
+    isQzone,
+    isOriginalChrome,
+    isSafari,
+  }
+}
+
+export { buildScheme, generateIntent, getBrowser }
