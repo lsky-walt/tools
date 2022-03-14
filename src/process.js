ef9bf14784a3eafe44bd97a46be6ca87c9e974c3
lsky-walt <lsky_walt@163.com>
2022-03-14 14:02:50 +0800

    feat: process

---
+++ src/process.js
@@ -0,0 +1,31 @@
+import { spawn } from "child_process"
+
+const exec = ({ command = "", args = [], option = [], log }) => {
+  return new Promise((resolve, reject) => {
+    if (!command || args.length <= 0) {
+      console.log()
+      console.log()
+      reject("参数不全，无法执行spawn命令...")
+      process.exit(1)
+    }
+    log && log(`执行命令：${command}`)
+    const child = spawn(command, args, { ...option })
+    let rs = ""
+    child.stdout &&
+      child.stdout.on("data", (msg) => {
+        rs += msg
+      })
+    child.on("error", (err) => {
+      reject(err)
+    })
+    child.on("close", (code) => {
+      if (code !== 0) {
+        reject("程序执行出错，请重新执行。")
+        return
+      }
+      resolve(rs)
+    })
+  })
+}
+
+export { exec }
